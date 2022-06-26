c8aa139d746b72ea58034abba848757703c69bd2
Mohamed Zeamari <zeamarim@MeinApfel.local>
2022-06-26 13:07:01 +0200

    Start and Finish Project

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -13,12 +13,31 @@ class ViewController: UIViewController {
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
+    var storyBrain = StoryBrain()
+    
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
+    }
+
+    @IBAction func choiceMade(_ sender: UIButton){
+
+        let userAnswer = sender.currentTitle
+
+        storyBrain.nextStory(userAnswer!)
+        updateUI()
 
     }
 
+    func updateUI(){
+        storyLabel.text = storyBrain.GetCurrentTitle()
+        choice1Button.setTitle(storyBrain.GetChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.GetChoice2(), for: .normal)
+
+    }
+    
 
 }
 
--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+
+struct Story {
+    let title : String
+    let choice1 : String
+    let choice2 : String
+    let choice1Destination : Int 
+    let choice2Destination : Int
+
+    
+    init(title: String, choice1:String,  choice1Destination: Int
+    , choice2:String, choice2Destination :Int) {
+        self.choice1 = choice1
+        self.choice2 = choice2
+        self.title = title
+        self.choice2Destination = choice2Destination
+        self.choice1Destination = choice1Destination
+    }
+    
+}
